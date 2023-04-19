e34ee088937f955d0559c22bd99eb123546371ed
Daniel Gilchrist <daniel@workforce.com>
2023-04-19 13:30:05 +0100

    Add worker and console

--- utils/open_work_tabs.py
+++ utils/open_work_tabs.py
@@ -36,17 +36,31 @@ class OpenWorkTabs:
   async def execute(self):
     main_pane = await self.__create_new_tab()
     main_session = main_pane.current_session
-    await self.__run_command(main_session, self.ssh_command)
 
-    repo_session = await self.__split_pane(main_session, True)
-    await self.__run_command(repo_session, "cdt")
+    # split vertically for console pane
+    console_session = await self.__split_pane(main_session, vertical=True)
+    await self.__ssh(console_session)
+    # Enter command but do not run - this allows time for syncer to finish
+    await self.__enter_command(console_session, "tanda-console")
 
-    sync_session = await self.__split_pane(repo_session, False)
+    # split horizontally for sync pane
+    sync_session = await self.__split_pane(console_session, vertical=False)
     await self.__run_command(sync_session, self.sync_command)
 
+    # back to first pane
     await main_session.async_activate()
-    await self.__run_command(main_session, self.export_db_command)
 
+    # split horizontally for worker pane
+    worker_session = await self.__split_pane(main_session, vertical=False)
+    await self.__ssh(worker_session)
+    # Enter command but do not run - this allows time for syncer to finish
+    await self.__enter_command(worker_session, "tanda-worker")
+
+    # back to first pane
+    await main_session.async_activate()
+
+    # ssh in for server
+    await self.__ssh(main_session)
     # Enter command but do not run - this allows time for syncer to finish
     await self.__enter_command(main_session, "tanda-server")
 
@@ -61,3 +75,10 @@ class OpenWorkTabs:
 
   async def __enter_command(self, session, command):
     return await session.async_send_text(command)
+
+  async def __export_db_type(self, session):
+    return await self.__run_command(session, self.export_db_command)
+
+  async def __ssh(self, session):
+    await self.__run_command(session, self.ssh_command)
+    await self.__export_db_type(session)
