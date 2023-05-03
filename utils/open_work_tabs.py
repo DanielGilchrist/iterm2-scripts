497c848c3e829bc5f3e2af4ca227b293cb7099a6
Daniel Gilchrist <danniieelg@gmail.com>
2023-05-03 12:07:28 +0100

    Add annotations when waiting for syncer (#2)

--- utils/open_work_tabs.py
+++ utils/open_work_tabs.py
@@ -3,6 +3,7 @@ import iterm2
 
 class OpenWorkTabs:
   SYNCER_READY_STR = "INFO: Ready!"
+  LOGIN_MATCH_STR = "ruby@"
 
   class _Commands:
     def ssh(self):
@@ -44,9 +45,6 @@ class OpenWorkTabs:
 
     # split vertically for console pane
     console_session = await self.__split_pane(main_session, vertical=True)
-    await self.__ssh(console_session)
-    # Enter command but do not run - this allows time for syncer to finish
-    await self.__enter_command(console_session, "tanda-console")
 
     # split horizontally for sync pane
     sync_session = await self.__split_pane(console_session, vertical=False)
@@ -58,22 +56,19 @@ class OpenWorkTabs:
 
     # back to first pane
     await main_session.async_activate()
-
     # split horizontally for worker pane
     worker_session = await self.__split_pane(main_session, vertical=False)
-    await self.__ssh(worker_session)
-    # Enter command but do not run - this allows time for syncer to finish
-    await self.__enter_command(worker_session, "tanda-worker")
 
     # back to first pane
     await main_session.async_activate()
 
-    # ssh in for server
-    await self.__ssh(main_session)
-    # Enter command but do not run - this allows time for syncer to finish
-    await self.__enter_command(main_session, "tanda-server")
-
     await current_tab.async_update_layout()
+
+    await self.__ssh_and_wait_for((
+      (main_session, "tanda-server"),
+      (console_session, "tanda-console"),
+      (worker_session, "tanda-worker")
+    ))
 
     await self.__wait_for_syncer(sync_session)
 
@@ -82,7 +77,19 @@ class OpenWorkTabs:
     await self.__run_command(console_session, "\n")
     await self.__run_command(worker_session, "\n")
 
-  async def __wait_for_syncer(self, session):
+  async def __ssh_and_wait_for(self, sessions_with_commands):
+    for session, command in sessions_with_commands:
+      await self.__ssh(session)
+      await self.__enter_command(session, command)
+
+    for session, command in sessions_with_commands:
+      await self.__wait_for_text(session, self.LOGIN_MATCH_STR)
+      await self.__annotate_wait_for_syncer(session, command)
+
+  async def __wait_for_text(self, session, text):
+    if await self.__text_already_exists(session, text):
+      return
+
     async with session.get_screen_streamer() as streamer:
       while True:
         screen_contents = await streamer.async_get()
@@ -91,8 +98,31 @@ class OpenWorkTabs:
 
         max_lines = screen_contents.number_of_lines
         for i in range(max_lines - 1):
-          if self.SYNCER_READY_STR in screen_contents.line(i).string:
+          if text in screen_contents.line(i).string:
             return
+
+        if await self.__text_already_exists(session, text):
+          return
+
+  async def __text_already_exists(self, session, text):
+    screen_contents = await session.async_get_screen_contents()
+    max_lines = screen_contents.number_of_lines
+    for i in range(max_lines - 1):
+      if text in screen_contents.line(i).string:
+        return True
+
+    return False
+
+  async def __wait_for_syncer(self, session):
+    await self.__wait_for_text(session, self.SYNCER_READY_STR)
+
+  async def __annotate_wait_for_syncer(self, session, text_to_match):
+    screen_contents = await session.async_get_screen_contents()
+    cursor_point = screen_contents.cursor_coord
+    from_ = iterm2.util.Point(cursor_point.x - len(text_to_match), cursor_point.y)
+    to = iterm2.util.Point(cursor_point.x, cursor_point.y)
+    point_range = iterm2.util.CoordRange(from_, to)
+    await session.async_add_annotation(point_range, "Waiting on syncer before running commands...")
 
   async def __create_new_tab(self):
     return await self.app.current_terminal_window.async_create_tab()
