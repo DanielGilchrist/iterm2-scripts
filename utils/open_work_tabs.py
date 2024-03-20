3d466fa306473a486678e6b0ba98f7f88ec2c08c
Daniel Gilchrist <danniieelg@gmail.com>
2024-03-20 11:48:46 +0000

    Update open work tabs script (#4)
    * WIP: Update open work tabs script
    
    * Fix issue where sometimes console wouldn't execute
    
    * Remove no longer necessary code
    
    * Make better use of async
    
    * Remove dead code
    
    * Resize panes, reorder panes + add visual representation of splitting vs not so useful comments
    
    * Avoid double bundle run
    
    * Fix issue where waiting for bundle on wrong session

--- utils/open_work_tabs.py
+++ utils/open_work_tabs.py
@@ -1,29 +1,19 @@
 import math
 import iterm2
+import asyncio
 
 class OpenWorkTabs:
   SYNCER_READY_STR = "INFO: Ready!"
-  LOGIN_MATCH_STR = "ruby@"
 
   class _Commands:
-    def ssh(self):
-      return self.SSH
-
-    def sync(self):
-      return self.SYNC
-
-    def db_type(self):
-      return self.EXPORT_TYPE
+    def tunnel(self):
+      return self.TUNNEL
 
   class _AUCommands(_Commands):
-    SSH = "tssh"
-    SYNC = "tsr"
-    EXPORT_TYPE = "apac"
+    TUNNEL = "REGION=apac bin/tunnel"
 
   class _EUCommands(_Commands):
-    SSH = "eutssh"
-    SYNC = "eutsr"
-    EXPORT_TYPE = "eu"
+    TUNNEL = "REGION=eu bin/tunnel"
 
   @classmethod
   def init_au(klass, app):
@@ -35,60 +25,90 @@ class OpenWorkTabs:
 
   def __init__(self, app, commands):
     self.app = app
-    self.ssh_command = commands.ssh()
-    self.sync_command = commands.sync()
-    self.export_db_command = f'export DB_CREDS_TYPE={commands.db_type()}'
+    self.tunnel_command = commands.tunnel()
 
   async def execute(self):
+    # _________
+    # |       |
+    # |       |
+    # |       |
+    # ---------
     current_tab = await self.__create_new_tab()
     main_session = current_tab.current_session
 
-    # split vertically for console pane
+    # _________
+    # |   |   |
+    # |   |   |
+    # |   |   |
+    # ---------
     console_session = await self.__split_pane(main_session, vertical=True)
 
-    # split horizontally for sync pane
-    sync_session = await self.__split_pane(console_session, vertical=False)
-    await self.__run_command(sync_session, self.sync_command)
-    sync_grid = sync_session.grid_size
-    # If we don't * 2 the width for some reason the width shrinks to about half
-    new_sync_size = iterm2.util.Size(sync_grid.width * 2, math.floor(sync_grid.height / 3.0))
-    sync_session.preferred_size = new_sync_size
+    # _________
+    # |   |   |
+    # |   |___|
+    # |   |   |
+    # ---------
+    webpack_session = await self.__split_pane(console_session, vertical=False)
 
-    # back to first pane
+    # back to first pane so we can split off it
     await main_session.async_activate()
-    # split horizontally for worker pane
+
+    # _________
+    # |   |   |
+    # |___|___|
+    # |   |   |
+    # ---------
     worker_session = await self.__split_pane(main_session, vertical=False)
 
-    # back to first pane
-    await main_session.async_activate()
+    # _________
+    # |   |   |
+    # |___|___|
+    # |___|   |
+    # ---------
+    tunnel_session = await self.__split_pane(worker_session, vertical=False)
 
+    await asyncio.gather(
+      self.__cdt(main_session),
+      self.__cdt(console_session),
+      self.__cdt(worker_session),
+      self.__cdt(webpack_session),
+      self.__cdt(tunnel_session),
+    )
+
+    # start tunnel
+    await self.__run_command(tunnel_session, self.tunnel_command)
+
+    await self.divide_grid_height(tunnel_session, 2.0)
+    await self.divide_grid_height(webpack_session, 5.0)
     await current_tab.async_update_layout()
 
-    await self.__ssh_and_wait_for((
-      (main_session, "tanda-server"),
-      (console_session, "tanda-console"),
-      (worker_session, "tanda-worker")
-    ))
+    # wait for tunnel to finish syncing
+    await self.__wait_for_text(tunnel_session, self.SYNCER_READY_STR)
 
-    await self.__wait_for_syncer(sync_session)
+    # run server and wait for bundle to finish before running the rest of the commands
+    await self.__run_command(main_session, "bin/dev server")
+    await self.__wait_for_text(main_session, "Connection to", " closed")
 
-    # run commands
-    await self.__run_command(main_session, "\n")
-    await self.__run_command(console_session, "\n")
-    await self.__run_command(worker_session, "\n")
+    # run all other commands that depend on bundle
+    await asyncio.gather(
+      self.__run_command(webpack_session, "bin/dev webpack"),
+      self.__run_command(worker_session, "bin/dev worker"),
+      self.__run_command(console_session, "bin/dev console"),
+    )
 
-  async def __ssh_and_wait_for(self, sessions_with_commands):
-    for session, command in sessions_with_commands:
-      await self.__ssh(session)
-      await self.__enter_command(session, command)
+  # This is janky AF and barely works
+  async def divide_grid_height(self, session, amount):
+    session_grid = session.grid_size
+    # If we don't * 2 the width for some reason the width shrinks to about half
+    new_grid_size = iterm2.util.Size(session_grid.width * 2, math.floor(session_grid.height / amount))
+    session.preferred_size = new_grid_size
 
-    for session, command in sessions_with_commands:
-      await self.__wait_for_text(session, self.LOGIN_MATCH_STR)
-      await self.__annotate_wait_for_syncer(session, command)
-
-  async def __wait_for_text(self, session, text):
+  async def __wait_for_text(self, session, text, second_text=None):
     if await self.__text_already_exists(session, text):
-      return
+      if second_text:
+        return self.__wait_for_text(session, second_text)
+      else:
+        return
 
     async with session.get_screen_streamer() as streamer:
       while True:
@@ -99,10 +119,16 @@ class OpenWorkTabs:
         max_lines = screen_contents.number_of_lines
         for i in range(max_lines - 1):
           if text in screen_contents.line(i).string:
-            return
+            if second_text:
+              return self.__wait_for_text(session, second_text)
+            else:
+              return
 
         if await self.__text_already_exists(session, text):
-          return
+          if second_text:
+            return self.__wait_for_text(session, second_text)
+          else:
+            return
 
   async def __text_already_exists(self, session, text):
     screen_contents = await session.async_get_screen_contents()
@@ -113,16 +139,13 @@ class OpenWorkTabs:
 
     return False
 
-  async def __wait_for_syncer(self, session):
-    await self.__wait_for_text(session, self.SYNCER_READY_STR)
-
-  async def __annotate_wait_for_syncer(self, session, text_to_match):
-    screen_contents = await session.async_get_screen_contents()
-    cursor_point = screen_contents.cursor_coord
-    from_ = iterm2.util.Point(cursor_point.x - len(text_to_match), cursor_point.y)
-    to = iterm2.util.Point(cursor_point.x, cursor_point.y)
-    point_range = iterm2.util.CoordRange(from_, to)
-    await session.async_add_annotation(point_range, "Waiting on syncer before running commands...")
+  # async def __annotate_text(self, session, text_to_match, annotation_text):
+  #   screen_contents = await session.async_get_screen_contents()
+  #   cursor_point = screen_contents.cursor_coord
+  #   from_ = iterm2.util.Point(cursor_point.x - len(text_to_match), cursor_point.y)
+  #   to = iterm2.util.Point(cursor_point.x, cursor_point.y)
+  #   point_range = iterm2.util.CoordRange(from_, to)
+  #   await session.async_add_annotation(point_range, annotation_text)
 
   async def __create_new_tab(self):
     return await self.app.current_terminal_window.async_create_tab()
@@ -133,16 +156,5 @@ class OpenWorkTabs:
   async def __run_command(self, session, command):
     await session.async_send_text(f'{command}\n')
 
-  async def __enter_command(self, session, command):
-    await session.async_send_text(command)
-
-  async def __export_db_type(self, session):
-    await self.__run_command(session, self.export_db_command)
-
-  async def __export_disable_spring(self, session):
-    await self.__run_command(session, "export DISABLE_SPRING=true")
-
-  async def __ssh(self, session):
-    await self.__run_command(session, self.ssh_command)
-    await self.__export_disable_spring(session)
-    await self.__export_db_type(session)
+  async def __cdt(self, session):
+    await self.__run_command(session, "cdt")
